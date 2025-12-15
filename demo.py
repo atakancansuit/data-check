ae269727e6e5b608ae6070a677f2a4b55754de44
Atakan Can <atakancan2000@gmail.com>
2025-12-15 16:46:06 +0300

    ilk odev bitti

--- demo.py
+++ demo.py
@@ -1,5 +1,9 @@
 
-
-def circle_area(radius):
+pi = 3.14159
+def circle_area(r):
     """Returns the area of the circle of given radius"""
-    pass  # YOUR CODE HERE
+    if r < 0:
+        return 0
+    A = pi * r ** 2
+    return A
+
